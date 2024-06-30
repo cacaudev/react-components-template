f9967d97e1dbe33574d45efb625690c0c46bacca
Claudia Rodrigues <cacauabreu2013@gmail.com>
2024-06-30 20:13:50 -0300

    feat: Make components responsive

--- src/ui/components/layout/content/RightSidebar.tsx
+++ src/ui/components/layout/content/RightSidebar.tsx
@@ -3,7 +3,27 @@ import React from "react";
 const RightSidebar: React.FC = () => {
     return (
       <div className="l-home__right-sidebar">
-        <p>RightSidebar</p>
+        <h1>RightSidebar</h1>
+        <article className="l-home__article">
+          <p className="l-home__article__paragraph">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Dignissimos laborum cumque incidunt, enim ipsa dicta? Porro illo
+            doloribus, consectetur eum exercitationem sit ipsam, est nesciunt
+            maxime, eius animi dolor? Harum.
+          </p>
+          <p className="l-home__article__paragraph">
+            Commodi culpa deserunt dolor sed, ipsam tempore ullam architecto
+            fuga voluptas, quis veniam. Vitae veniam, odit adipisci, quas
+            similique debitis excepturi quisquam minima facere temporibus, porro
+            optio perferendis iusto fuga.
+          </p>
+          <p className="l-home__article__paragraph">
+            Illo numquam, sapiente neque repellendus facere amet doloribus
+            asperiores quia eum? Sunt vero amet neque vel? Tempora, nulla
+            voluptatum amet autem culpa magnam debitis! Dolores esse quam amet
+            nobis ut.
+          </p>
+      </article>
       </div>
     );
   };
