e27b78a5cded4ec19496fdb3677edd34cc88dbbf
Claudia Rodrigues <cacauabreu2013@gmail.com>
2024-07-04 17:54:39 -0300

    fix: Fix a few details

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { Home } from "./ui/components/views/home/Home";
 import { Footer } from "./ui/components/layout/footer/Footer";
 import { Dashboard } from "./ui/components/views/dashboard/Dashboard";
 import { useSidebarContext } from "./infra/controllers/context/SidebarToggleContext";
+import useResponsiveWidth from "./infra/hooks/WindowDimensionsHook";
 
-function App() {  
+function App() {
   const { isSidebarCollapsed } = useSidebarContext();
-  
   return (
     <div className="App">
-      <div className={`l-page  ${isSidebarCollapsed ? "c-nav-collapsed" : ""}`}>
+      <div
+        className={`l-page ${isSidebarCollapsed ? "c-nav-collapsed" : ""} ${useResponsiveWidth() < 768 ? 'is-tablet' : ''}`}
+      >
         <Navbar />
         <div className="l-page__content">
           <Routes>
